cf10bc4d5cadc63225640f144ac41f444756021a
Mohammad Shuaib <mohammadshuaib108@gmail.com>
2024-07-29 20:53:24 +0530

    Second commit

--- script.js
+++ script.js
@@ -26,9 +26,22 @@ let Arrproducts = [
 ]
 
 const body = document.querySelector("body"),
-products = document.querySelector(".products");
+products = document.querySelector(".products"),
+shoppingBasket = document.querySelector(".shoppingBasket"),
+closeCart = document.querySelector(".close"),
+productList = document.querySelector(".productList"),
+quantity = document.querySelector(".quantity"),
+total = document.querySelector(".total");
 
 let checkoutList = [];
+
+shoppingBasket.onclick = () => {
+    body.classList.add("active");
+};
+
+closeCart.onclick = () => {
+    body.classList.remove("active");
+};
 
 function onInIt(){
     Arrproducts.forEach((item,key) => {
@@ -49,5 +62,42 @@ onInIt();
 function addtoCart(id){
     if(checkoutList[id] == null){
         checkoutList[id] = Arrproducts[id];
+        checkoutList[id].quantity = 1;
+    }else{
+        checkoutList[id].quantity += 1;
     }
+    refreshCart();
+}
+
+function refreshCart(){
+    productList.innerHTML=``;
+    let count = 0;
+    let totalPrice = 0;
+    checkoutList.forEach((item,key) =>{
+        count += item.quantity;
+        totalPrice=parseInt(item.Price * item.quantity);
+        let li = document.createElement("li");
+        li.innerHTML=
+        `<img src="images/${item.image}">
+        <div>${item.name}</div>
+        <div>${item.Price}</div>
+        <div>
+        <button onclick="changeQuantity(${key},${item.quantity - 1})">-</button>
+        <div class="count">${item.quantity}</div>
+        <button onclick="changeQuantity(${key},${item.quantity + 1})">+</button>`;
+
+        productList.appendChild(li);
+        
+        quantity.innerHTML = count;
+        // total.innerHTML = `<small>Subtotal(${count} items)</small>`+totalPrice;
+    });
+}
+
+function changeQuantity(key, quantity){
+    if(quantity  == 0){
+        delete checkoutList[key];
+    }else{
+        checkoutList[key].quantity=quantity;
+    }
+    refreshCart();
 }
